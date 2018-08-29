6fd36ab36e74e39b46be9d29fdce2b8b27beb830
pkmitl205 <58030205@kmitl.ac.th>
2018-08-29 20:55:09 +0700

    Ex19

--- 29082561_Ex1/29082561_Ex1/Program.cs
+++ 29082561_Ex1/29082561_Ex1/Program.cs
@@ -10,14 +10,8 @@ namespace _29082561_Ex1
     {
         static void Main(string[] args)
         {
-            // Ex12
-            double myDouble = 12.345678;
-            Console.WriteLine("{0, -10:G} -- General", myDouble);   // 12.345678 -- General
-            Console.WriteLine("{0, -10} -- Default, same as General", myDouble); // 12.345678 -- Default, same as General
-            Console.WriteLine("{0, -10:F4} -- Fixed Point, 4 dec places", myDouble); // 12.3457 -- Fixed Point, 4 dec places
-            Console.WriteLine("{0, -10:C} -- Currency", myDouble); // ฿12.35 -- Currency
-            Console.WriteLine("{0, -10:E3} -- Sci. Notation, 3 dec places", myDouble); // 1.235E+001-- Sci.Notation, 3 dec places
-            Console.WriteLine("{0, -10:x} -- Hexadecimal integer", 1194719); // 123adf -- Hexadecimal integer
+            // Ex19
+            Console.WriteLine("{0 :P2}", 0.1221897); // 12.22%
         }
     }
 }
