8573aaafa28298a5777ef8961d18d9255bf211b6
pkmitl205 <58030205@kmitl.ac.th>
2018-08-29 20:18:27 +0700

    Ex14

---
+++ 29082561_Ex1/29082561_Ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29082561_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ex14
+            Console.WriteLine("{0 :D4}", 12);
+        }
+    }
+}
